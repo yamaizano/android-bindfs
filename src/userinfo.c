f51b76a91c779f77e1dd050e459ee551c9cd5c42
Martin Pärtel <martin.partel@gmail.com>
2020-04-13 13:16:07 +0300

    On UID/GID DB read failure, print non-debug message and tolerate ENOENT.

--- src/userinfo.c
+++ src/userinfo.c
@@ -124,7 +124,11 @@ static int rebuild_uid_cache()
         if (pw == NULL) {
             if (errno == 0) {
                 break;
+            } else if (errno == ENOENT) {  // We might have gotten some entries. This happens at least on the CentOS 8 Vagrant image (tested 2020-04-13).
+                fprintf(stderr, "Got ENOENT while rebuilding uid cache. The cache may be incomplete.\n");
+                break;
             } else {
+                perror("Failed to rebuild uid cache");
                 goto error;
             }
         }
@@ -146,7 +150,6 @@ static int rebuild_uid_cache()
 error:
     endpwent();
     clear_uid_cache();
-    DPRINTF("Failed to rebuild uid cache");
     return 0;
 }
 
@@ -170,7 +173,11 @@ static int rebuild_gid_cache()
         if (gr == NULL) {
             if (errno == 0) {
                 break;
+            } else if (errno == ENOENT) {  // We might have gotten some entries. This happens at least on the CentOS 8 Vagrant image (tested 2020-04-13).
+                fprintf(stderr, "Got ENOENT while rebuilding gid cache. The cache may be incomplete.\n");
+                break;
             } else {
+                perror("Failed to rebuild gid cache");
                 goto error;
             }
         }
@@ -204,7 +211,6 @@ static int rebuild_gid_cache()
 error:
     endgrent();
     clear_gid_cache();
-    DPRINTF("Failed to rebuild uid cache");
     return 0;
 }
 
